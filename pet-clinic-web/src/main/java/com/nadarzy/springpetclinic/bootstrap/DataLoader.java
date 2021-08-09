38a7ae92bf62790012492654dd33d70c62d3a2d8
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-08-09 17:13:47 +0200

    added 2 pet types to bootstrap data . closes #30.

--- pet-clinic-web/src/main/java/com/nadarzy/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/nadarzy/springpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.nadarzy.springpetclinic.bootstrap;
 
 import com.nadarzy.springpetclinic.model.Owner;
+import com.nadarzy.springpetclinic.model.PetType;
 import com.nadarzy.springpetclinic.model.Vet;
 import com.nadarzy.springpetclinic.services.OwnerService;
+import com.nadarzy.springpetclinic.services.PetTypeService;
 import com.nadarzy.springpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,27 @@ public class DataLoader implements CommandLineRunner {
 
   private final OwnerService ownerService;
   private final VetService vetService;
+  private final PetTypeService petTypeService;
 
-  public DataLoader(OwnerService ownerService, VetService vetService) {
+  public DataLoader(
+      OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
     this.ownerService = ownerService;
     this.vetService = vetService;
+    this.petTypeService = petTypeService;
   }
 
   @Override
   public void run(String... args) throws Exception {
+
+    PetType dog = new PetType();
+    dog.setName("Dog");
+
+    PetType savedDogType = petTypeService.save(dog);
+
+    PetType cat = new PetType();
+    cat.setName("Cat");
+
+    PetType savedCatType = petTypeService.save(cat);
 
     Owner owner1 = new Owner();
 
