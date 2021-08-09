01a67f9953531a92d67d9f3aeaa96a65fe9f5115
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-08-09 16:55:08 +0200

    added Specialty, added Specialty relationship to Vets . closes #25.

---
+++ pet-clinic-data/src/main/java/com/nadarzy/springpetclinic/model/Specialty.java
@@ -0,0 +1,16 @@
+package com.nadarzy.springpetclinic.model;
+
+import java.io.Serializable;
+
+public class Specialty extends BaseEntity implements Serializable {
+
+  private String description;
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
