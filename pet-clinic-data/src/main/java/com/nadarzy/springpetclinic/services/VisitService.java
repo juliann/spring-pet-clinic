165b8f90b83bd6c32ff80e6cb673b3a7ade7e4e0
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-08-12 10:42:25 +0200

    added VisitService + Visit Map Service. closes #44.

---
+++ pet-clinic-data/src/main/java/com/nadarzy/springpetclinic/services/VisitService.java
@@ -0,0 +1,5 @@
+package com.nadarzy.springpetclinic.services;
+
+import com.nadarzy.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {}
---
+++ pet-clinic-data/src/main/java/com/nadarzy/springpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,36 @@
+package com.nadarzy.springpetclinic.services.map;
+
+import com.nadarzy.springpetclinic.model.Visit;
+import com.nadarzy.springpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+  @Override
+  public Set<Visit> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public Visit findById(Long id) {
+    return super.findById(id);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+
+  @Override
+  public void delete(Visit t) {
+    super.delete(t);
+  }
+
+  @Override
+  public Visit save(Visit t) {
+    if (t.getPet() == null || t.getPet().getOwner() == null || t.getPet().getId() == null) {
+      throw new RuntimeException("invalid visit");
+    }
+    return super.save(t);
+  }
+}
