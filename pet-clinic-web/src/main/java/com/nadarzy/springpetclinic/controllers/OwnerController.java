87461c7514d8f4623715c36527f274032b747455
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-08-19 11:02:32 +0200

    bug on display owner, wrong types (petType). references #59

--- pet-clinic-web/src/main/java/com/nadarzy/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/nadarzy/springpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.nadarzy.springpetclinic.controllers;
 import com.nadarzy.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -22,8 +25,10 @@ public class OwnerController {
     return "owners/index";
   }
 
-  @RequestMapping("/find")
-  public String findOwners() {
-    return "notimplemented";
+  @GetMapping("/{ownerId}")
+  public ModelAndView findOwners(@PathVariable Long ownerId) {
+    ModelAndView view = new ModelAndView("owners/ownerDetails");
+    view.addObject(this.ownerService.findById(ownerId));
+    return view;
   }
 }
