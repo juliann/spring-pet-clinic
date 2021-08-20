385faa0954f8f2b0ca6abfe2d92d5345c50205f7
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-08-20 10:54:08 +0200

    adding and updating visits. closes #55

--- pet-clinic-data/src/main/java/com/nadarzy/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/nadarzy/springpetclinic/model/Pet.java
@@ -37,6 +37,6 @@ public class Pet extends BaseEntity {
     this.birthDate = birthDate;
     this.petType = petType;
     this.owner = owner;
-    this.visits = visits;
+    if (visits == null || visits.size() > 0) this.visits = visits;
   }
 }
